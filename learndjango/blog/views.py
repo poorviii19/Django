b7f4e66ad0bd38323a0a87176d917de42b86fcae
poorviii19 <shuklapoorviswe@gmail.com>
2026-08-05 11:26:06 +0530

    create another view and its url

--- learndjango/blog/views.py
+++ learndjango/blog/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("<h1> Welcome to my Blog!</h1>")
+
+
+def about(request):
+    return HttpResponse("<h1> About Us:<h1> <p> This is django's blog</p>")
