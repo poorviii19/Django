9303532c22ce1c2868c5f503c2211625808e12d0
poorviii19 <shuklapoorviswe@gmail.com>
2026-08-26 11:02:29 +0530

    Implement re_path with regex

--- learndjango/blog/urls.py
+++ learndjango/blog/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('greet/<int:name>/', views.greet),
     path('movieFinder/<str:movie>/', views.movieFinder),
     path('recipe/', views.recipe, name='recipe'),
+    # re_path(r'^user/(?P<username>[a-zA-Z]+)/$' ,views.userProfile),
+    re_path(r'^user/(?P<username>[a-zA-Z]*)/?$' ,views.userProfile),
 ]
